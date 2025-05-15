a2acae67698f7eb8046024d517a93058330b2edd
AryaBagusPutraPratama <bagusaria14@gmail.com>
2025-05-15 20:33:03 +0700

    Login and Register fetching

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'sanctum', // Pastikan ini
+            'provider' => 'users',
+            ],
     ],
 
     /*
