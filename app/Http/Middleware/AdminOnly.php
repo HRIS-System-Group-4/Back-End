072b5f81ecbab6499f8b9d3f2c1d143eaac22c50
GalaAldebara <lalasipo20@gmail.com>
2025-05-30 23:47:56 +0700

    middleware admin & employee

--- app/Http/Middleware/AdminOnly.php
+++ app/Http/Middleware/AdminOnly.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminOnly
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!$request->user() || !$request->user()->is_admin) {
+            return response()->json([
+                'message' => 'Access denied. Admins only.'
+            ], 403);
+        }
+
         return $next($request);
     }
 }
